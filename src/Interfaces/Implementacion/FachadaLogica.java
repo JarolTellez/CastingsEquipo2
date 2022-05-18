f0be5c9cb85adcbf86c935dab1e21fb877ff05f8
DESKTOP-TN3BH2C\velas <dianey.velasquez228270@potros.itson.edu.mx>
2022-05-17 17:50:44 -0700

    Progreso de Perfil 2

--- src/Interfaces/Implementacion/FachadaLogica.java
+++ src/Interfaces/Implementacion/FachadaLogica.java
@@ -11,43 +11,32 @@ import ObjetoNegocio.*;
 import java.util.Date;
 import java.util.List;
 
-
 /**
  *
  * @author Jarol
  */
 public class FachadaLogica implements ILogica {
-    
-    CtrClientes controlClientes= new CtrClientes();
-    CtrlCastings controlCastings= new CtrlCastings();
-    CtrAgente controlAgente= new CtrAgente();
-    CtrPerfil controlPerfil=new CtrPerfil();
 
+    CtrClientes controlClientes = new CtrClientes();
+    CtrlCastings controlCastings = new CtrlCastings();
+    CtrAgente controlAgente = new CtrAgente();
+    CtrPerfil controlPerfil = new CtrPerfil();
 
-   
     @Override
     public void registrarCliente(Cliente cliente) {
-        try
-        {
-        if(controlClientes.buscarClienteNombre(cliente.getNombre())==null)
-        {
-       controlClientes.Registrar(cliente);
-       
-       
-        }else
-        {
-            throw new RuntimeException("Cliente Existente");
-           // return new Exception("Cliente Existente");
+        try {
+            if (controlClientes.buscarClienteNombre(cliente.getNombre()) == null) {
+                controlClientes.Registrar(cliente);
+
+            } else {
+                throw new RuntimeException("Cliente Existente");
+                // return new Exception("Cliente Existente");
+            }
+
+        } catch (RuntimeException ex) {
+            throw new RuntimeException(ex.getMessage());
         }
-       
-        
-        }catch(RuntimeException ex)
-        {
-               throw new RuntimeException(ex.getMessage());
-        }
-        
-      
-        
+
     }
 
     @Override
@@ -57,13 +46,19 @@ public class FachadaLogica implements ILogica {
 
     @Override
     public List<Cliente> consultarTodosClientes() {
-      
+
         return controlClientes.consultarTodos();
     }
 
     @Override
+    public List<Perfil> consultarTodosPerfiles() {
+
+        return controlPerfil.consultarTodos();
+    }
+
+    @Override
     public List<Agente> consultarTodosAgentes() {
-       return controlAgente.consultarTodos();
+        return controlAgente.consultarTodos();
     }
 
     @Override
@@ -78,7 +73,7 @@ public class FachadaLogica implements ILogica {
 
     @Override
     public void agregarFase() {
-      
+
     }
 
     @Override
@@ -88,22 +83,17 @@ public class FachadaLogica implements ILogica {
 
     @Override
     public void guardarCasting(Presencial casting) {
-        try
-        {
-            if(controlCastings.buscarCastingNombre(casting.getNombre())==null)
-            {
-                       controlCastings.guardar(casting);
-            }else
-            {
+        try {
+            if (controlCastings.buscarCastingNombre(casting.getNombre()) == null) {
+                controlCastings.guardar(casting);
+            } else {
                 throw new RuntimeException("Casting existente");
             }
-        }catch(RuntimeException ex)
-        {
-             throw new RuntimeException(ex.getMessage());
+        } catch (RuntimeException ex) {
+            throw new RuntimeException(ex.getMessage());
         }
-       
+
     }
-    
 
     @Override
     public void guardarPerfil(Perfil pefil) {
@@ -112,53 +102,36 @@ public class FachadaLogica implements ILogica {
 
     @Override
     public Casting consultarCastingNombre(String nombre) {
-      try{
-                return controlCastings.buscarCastingNombre(nombre);
-            
-    
-        }catch(RuntimeException ex)
-                {
-                    throw new RuntimeException(ex.getMessage());
-                }
+        try {
+            return controlCastings.buscarCastingNombre(nombre);
+
+        } catch (RuntimeException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
     }
 
     @Override
     public void registrarAgente(Agente agente) {
-          try
-        {
-            if(controlAgente.buscarAgenteNombre(agente.getNombre())==null)
-            {
+        try {
+            if (controlAgente.buscarAgenteNombre(agente.getNombre()) == null) {
                 controlAgente.Registrar(agente);
+            } else {
+                throw new RuntimeException("Agente Existente");
+                // return new Exception("Agente Existente");
             }
-            
-        else
-        {
-            throw new RuntimeException("Agente Existente");
-           // return new Exception("Agente Existente");
+
+        } catch (RuntimeException ex) {
+            throw new RuntimeException(ex.getMessage());
         }
-       
-        
-        }catch(RuntimeException ex)
-        {
-               throw new RuntimeException(ex.getMessage());
-        }  
     }
 
     @Override
     public List<Presencial> consultarTodosCasting(String nombre, String codigo, Date fecha) {
-        if(controlCastings.consultarTodosCasting(nombre, codigo, fecha).size()<1)
-        {
+        if (controlCastings.consultarTodosCasting(nombre, codigo, fecha).size() < 1) {
             throw new RuntimeException("Casting no existente");
-        }else
-        {
+        } else {
             return controlCastings.consultarTodosCasting(nombre, codigo, fecha);
         }
     }
 
-    
-    
-   
-   
-  
-    
 }
